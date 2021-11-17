6f6d1489a3540d7bfd276f842a1e69e016ec31f5
Richard Scott <github@pointb.co.uk>
2021-11-17 17:07:41 +0000

    added dynamic config aliases

--- Dockerfile
+++ Dockerfile
@@ -17,8 +17,8 @@ RUN echo \
  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
  && rm -f /etc/apt/mirror.list \
  && mv -f /var/spool/apt-mirror/wgetrc /etc/wgetrc \
- && mv /var/spool/apt-mirror/mirror.list /etc/apt/mirror.list \
- && mv /var/spool/apt-mirror/sites-enabled_default /etc/nginx/sites-enabled/default 
+ && mv /var/spool/apt-mirror/mirror.list /etc/apt/mirror.list
+
 EXPOSE 80
 
 VOLUME ["/var/spool/apt-mirror"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 sync_flag="/var/spool/apt-mirror/sync"
-#cat /etc/apt/mirror.list | egrep "^clean\s" | awk -F '//' '{print $2}'
+rm -f /etc/nginx/sites-enabled/default
+cat <<EOF > /etc/nginx/conf.d/${SITE}.conf
+server {
+  listen 80 default_server;
+  server_name _;
+
+  access_log /dev/stdout;
+  error_log  /dev/stdout;
+
+  server_name_in_redirect off;
+
+EOF
+for DIR in $(cat /etc/apt/mirror.list | egrep '^clean\s' | awk -F '//' '{print $2}') ; do
+  SITE="$(echo ${DIR} | awk -F '/' '{print $1}')"
+  NAME="$(echo ${DIR} | awk -F '/' '{print $2}')"
+  echo "  location /${NAME} {" >> /etc/nginx/conf.d/${SITE}.conf
+  echo "      alias /var/spool/apt-mirror/mirror/${DIR};" >> /etc/nginx/conf.d/${SITE}.conf
+  echo "      autoindex on;" >> /etc/nginx/conf.d/${SITE}.conf
+  echo "  }" >> /etc/nginx/conf.d/${SITE}.conf
+done
+cat <<EOF >> /etc/nginx/conf.d/${SITE}.conf
+}
+EOF
 nginx
 while true ; do
   if [[ -e ${sync_flag} ]]; then
