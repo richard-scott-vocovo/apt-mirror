d50b48316320967fd036fcd8e01a03a418c78555
Richard Scott <github@pointb.co.uk>
2021-11-17 16:37:45 +0000

    deleted nginx config file

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sync_flag="/var/spool/apt-mirror/sync"
+#cat /etc/apt/mirror.list | egrep "^clean\s" | awk -F '//' '{print $2}'
 nginx
 while true ; do
   if [[ -e ${sync_flag} ]]; then
     /usr/bin/apt-mirror
     rm -f ${sync_flag}
   else
-    sleep 600s
+    sleep 60s
   fi
 done
